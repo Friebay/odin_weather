15f2730a867f74421fced136e4d452aca9a83a7b
Friebay <frybaylt@gmail.com>
2024-11-26 12:23:03 +0200

    update 403 error

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const API_KEY = 'BUVXA3ERKWD8HN9CZAXXGGDB6';
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 const BASE_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
 
 export const fetchWeatherData = async (location, unit = 'metric') => {
     try {
-        const url = `${BASE_URL}/${location}/today?unitGroup=${unit}&include=current&key=${API_KEY}&contentType=json`;
+        const url = `${BASE_URL}/${encodeURIComponent(location)}/today?unitGroup=${unit}&include=current&key=${API_KEY}&contentType=json`;
         
-        const response = await axios.get(`${CORS_PROXY}${url}`, {
+        const response = await axios.get(url, {
             headers: {
-                'X-Requested-With': 'XMLHttpRequest'
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             }
         });
         
@@ -25,8 +25,20 @@ export const fetchWeatherData = async (location, unit = 'metric') => {
             description: response.data.currentConditions.conditions
         };
     } catch (error) {
-        console.error('Error fetching weather data:', error);
-        throw error;
+        console.error('Error fetching weather data:', error.response ? error.response.data : error.message);
+        
+        // Provide a more detailed error message
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            throw new Error(`API Error: ${error.response.status} - ${JSON.stringify(error.response.data)}`);
+        } else if (error.request) {
+            // The request was made but no response was received
+            throw new Error('No response received from the server');
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            throw new Error(`Request setup error: ${error.message}`);
+        }
     }
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/odin_weather/' // Important for GitHub Pages
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: isProduction ? '/odin_weather/' : '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 3000,
-    open: true
-  }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development')
+      })
+    ],
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      port: 3000,
+      open: true
+    }
+  };
 };
